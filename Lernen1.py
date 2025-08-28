751916231f8a700c3a194af743d4d29f4f85ab62
Hashem <yasenh147@gmail.com>
2025-08-28 22:42:26 +0200

    Lernen1

---
+++ Lernen1.py
@@ -0,0 +1,21 @@
+print('test Python')
+print("*" * 10)
+
+nummer = 10
+name = "Yassin"
+
+print(nummer)
+print(name)
+
+print(type(nummer))
+print(type(name))
+
+# >format dokument  um die Code klar schreiben oder bei Sitting formatOnsave
+x = 5
+
+# strg + r um file zu run
+
+#  Jython ist java code in Python Programm
+#  Cpython ist c code in Python Programm
+
+# Kurs von Youtube :https://www.youtube.com/watch?v=K5KVEU3aaeQ    , Zeit: 31:48
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Hier ist voll Kurs über python :
 - KI
 - Maschien Lerning
 - Web Entwicklung
+
+Code mit Python 3
